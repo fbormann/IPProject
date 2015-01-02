027ac13efff66c651a1064eeb242ba92b024e56b
fhab <fhab@p1c04.windows.cin.ufpe.br>
2015-01-02 12:16:16 -0300

    teste #merge com outra branch.

--- projeto/src/dados/ServicoRepositorioArray.java
+++ projeto/src/dados/ServicoRepositorioArray.java
@@ -10,37 +10,30 @@ public class ServicoRepositorioArray implements ServicoRepositorio{
 	}
 
 	public void adicionar(Servico servico) {
-		// TODO Auto-generated method stub
-		
+		Servico[] aux = new Servico[this.servicos.length+1];
 	}
 
 	public void remover(String ID) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public void atualizar(String ID) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public double consultaPreco(String ID) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public double consultaPrecoNome(String nome) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public Servico[] listar() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public Servico consulta(String ID) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	

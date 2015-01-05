54ff67852111fba6f4e4c8123c97c2ba894fa926
fhab <fhab@p1c02.windows.cin.ufpe.br>
2015-01-05 16:53:42 -0300

    #1 A GUI não mais se comunica com a camada de negocios diretamente, agora utilizamos a fachada chamada "Oficina". #2 Projeto agora possui de fato todas as camadas necessárias para a modularização. #3 CadastrarCliente não é mais responsável por verificações

--- projeto/src/comunicacao/Oficina.java
+++ projeto/src/comunicacao/Oficina.java
@@ -2,6 +2,10 @@ package comunicacao;
 
 import dados.RepositorioComprasArray;
 import entidades.Compra;
+import entidades.Conta;
+import excecoes.CEPInvalidoException;
+import excecoes.ContaJaCadastradaException;
+import excecoes.PlacaInvalida;
 import negocios.*;
 
 public class Oficina {
@@ -24,5 +28,81 @@ public class Oficina {
 		}
 		
 		this.compras.cadastrarCompra(compra);
+	}
+	
+	public void adicionarConta(Conta conta){
+		if(!this.contas.exist(conta.getCPF())){
+			try {
+				this.contas.cadastrar(conta);
+			} catch (ContaJaCadastradaException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} //TODO devo usar try catch aqui na camada de fachada ou na propria GUI?
+		}else{
+			//TODO: Implementar acoes contra.
+		}
+	}
+	
+	public boolean validadeCEP(String CEP) throws CEPInvalidoException{
+		CEPInvalidoException e = new CEPInvalidoException(CEP);
+		//checar tamanho
+		if(CEP.length()!=9){
+			throw e;
+			//checando se há somente números
+		}else{
+			for(int i = 0; i<CEP.length(); i++){
+				if(i!=5){
+					if(CEP.charAt(i)>=48 && CEP.charAt(i)<=57){
+						return true;
+					}else{
+						throw e;
+					}
+				}
+			}
+			//checando ap
+			String exemploCEP = "00000-000";
+			int a = exemploCEP.charAt(5);
+			if(a == 45){
+				return true;
+			}else{
+				throw e;
+			}
+		}
+	}
+
+	public boolean validadePlaca(String placa) throws PlacaInvalida{
+		PlacaInvalida a = new PlacaInvalida(placa);
+		//checar tamanho
+		if(placa.length()!=8){
+			throw a;
+			//checando se há numeros onde deve haver números
+		}else{
+			for(int i = 0; i <placa.length(); i++){
+				if(i>=4){
+					if(placa.charAt(i)>=48 && placa.charAt(i)<=57){
+						return true;
+					}else{
+						throw a;
+					}
+				}
+				//checando se há letras onde deve haver letras
+				if(i<=2){
+					if(placa.charAt(i)>=65 && placa.charAt(i)<=90){
+						return true;
+					}else{
+						throw a;
+					}
+				}
+			}
+			//checando se o hifen está na posicao correta
+			String exemploPlaca = "AAA-0000";
+			int c = exemploPlaca.charAt(3);
+			if(c == 45){
+				return true;
+			}else{
+				throw a;
+			}
+
+		}
 	}
 }
--- projeto/src/gui/CadastrarClientes.java
+++ projeto/src/gui/CadastrarClientes.java
@@ -35,13 +35,14 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JScrollBar;
 
+import comunicacao.Oficina;
+
 public class CadastrarClientes extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField tf_name;
 	private JTextField tf_cpf;
-	private CadastroConta contas;
-	private RepositorioContasArray repositorioContas;
+	private Oficina oficina;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_3;
@@ -79,8 +80,7 @@ public class CadastrarClientes extends JFrame {
 	 */
 	//TODO: criar construtor para testar a janela do cadastrar já preenchida com as informações do cliente (para poder editar) para não termos que fazer outra janela igual
 	public CadastrarClientes() {
-		repositorioContas = new RepositorioContasArray();
-		contas = new CadastroConta(repositorioContas);
+		oficina = new Oficina();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -116,11 +116,7 @@ public class CadastrarClientes extends JFrame {
 				Conta novaConta = new Conta();
 				novaConta.setCPF(cpf);
 				novaConta.setNome(nome);
-				try {
-					contas.cadastrar(novaConta);
-				} catch (ContaJaCadastradaException e1) {
-					e1.getMessage(); //TODO: create popup to display message.
-				}
+				oficina.adicionarConta(novaConta);
 			}
 		});
 		btnCadastrar.setBounds(320, 228, 89, 23);
@@ -243,97 +239,8 @@ public class CadastrarClientes extends JFrame {
 		});
 	}
 
-	public boolean validadeCPF(String CPF) throws CPFInvalidoException{
-		CPFInvalidoException e = new CPFInvalidoException(CPF);
-		//checar tamanho da entrada
-		if(CPF.length()!=14){
-			throw e;
-		}else{
-			//checar as posicoes que devem ter numeros
-			for(int i = 0; i<CPF.length(); i++){
-				if(i!=3 && i!=7 && i!=11){
-					if(CPF.charAt(i)>=48 && CPF.charAt(i)<=57){
-						return true;
-					} else {
-						throw e;
-					}
-				}
-			}
-			//checar os pontos e o hifen
-			String exemploCPF = "000.000.000-00";
-			int a = exemploCPF.charAt(3);
-			int b = exemploCPF.charAt(7);
-			int c = exemploCPF.charAt(11);
-			if(a == 46 && b == 46 && c == 45){
-				return true;
-			}else{
-				throw e;
-			}
-		}
-	}
+	
 
-	public boolean validadeCEP(String CEP) throws CEPInvalidoException{
-		CEPInvalidoException e = new CEPInvalidoException(CEP);
-		//checar tamanho
-		if(CEP.length()!=9){
-			throw e;
-			//checando se há somente números
-		}else{
-			for(int i = 0; i<CEP.length(); i++){
-				if(i!=5){
-					if(CEP.charAt(i)>=48 && CEP.charAt(i)<=57){
-						return true;
-					}else{
-						throw e;
-					}
-				}
-			}
-			//checando ap
-			String exemploCEP = "00000-000";
-			int a = exemploCEP.charAt(5);
-			if(a == 45){
-				return true;
-			}else{
-				throw e;
-			}
-		}
-	}
-
-	public boolean validadePlaca(String placa) throws PlacaInvalida{
-		PlacaInvalida a = new PlacaInvalida(placa);
-		//checar tamanho
-		if(placa.length()!=8){
-			throw a;
-			//checando se há numeros onde deve haver números
-		}else{
-			for(int i = 0; i <placa.length(); i++){
-				if(i>=4){
-					if(placa.charAt(i)>=48 && placa.charAt(i)<=57){
-						return true;
-					}else{
-						throw a;
-					}
-				}
-				//checando se há letras onde deve haver letras
-				if(i<=2){
-					if(placa.charAt(i)>=65 && placa.charAt(i)<=90){
-						return true;
-					}else{
-						throw a;
-					}
-				}
-			}
-			//checando se o hifen está na posicao correta
-			String exemploPlaca = "AAA-0000";
-			int c = exemploPlaca.charAt(3);
-			if(c == 45){
-				return true;
-			}else{
-				throw a;
-			}
-
-		}
-	}
 }
 
 

ed89f7ed55a04042ad6554ef442cb783d6703970
unknown <fhab@p1c04.windows.cin.ufpe.br>
2015-01-15 16:50:08 -0300

    #1 GUI Layout de AdicionarCompra.java foi finalizada.

--- projeto/src/gui/AdicionarCompra.java
+++ projeto/src/gui/AdicionarCompra.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+
+import comunicacao.OficinaFacade;
+
+import entidades.Conta;
+import entidades.Servico;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AdicionarCompra extends JFrame {
 
@@ -39,7 +48,7 @@ public class AdicionarCompra extends JFrame {
 	 */
 	public AdicionarCompra() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 477, 432);
+		setBounds(100, 100, 477, 461);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -47,6 +56,17 @@ public class AdicionarCompra extends JFrame {
 		
 		JList list_disponiveis = new JList();
 		list_disponiveis.setBounds(27, 88, 173, 229);
+		
+		final DefaultListModel model_Disponiveis = new DefaultListModel();
+		if(OficinaFacade.servicosArray != null){
+			Servico[] servicos = OficinaFacade.servicosArray.listar();
+			if(servicos != null){
+				for(int i = 0; i < servicos.length;i++){
+					model_Disponiveis.addElement(servicos[i].getNome() + "[" + servicos[i].getID() +"]" + " " + servicos[i].getPreco());
+				}
+			}
+		}
+		list_disponiveis.setModel(model_Disponiveis);
 		contentPane.add(list_disponiveis);
 		
 		JLabel lblListaDeServios = new JLabel("Lista de Servi\u00E7os Dispon\u00EDveis");
@@ -62,10 +82,23 @@ public class AdicionarCompra extends JFrame {
 		contentPane.add(lblListaDeServios_1);
 		
 		JButton btnFinalizarCompra = new JButton("Finalizar Compra");
-		btnFinalizarCompra.setBounds(290, 360, 136, 23);
+		btnFinalizarCompra.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO: Finalizar Compra
+				//TODO: Pegar a lista de Servicos e adiciona à compra.
+				//TODO: Checar se o CPF é válido antes de cadastrar.
+			}
+		});
+		btnFinalizarCompra.setBounds(290, 375, 136, 23);
 		contentPane.add(btnFinalizarCompra);
 		
 		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new Compras().setVisible(true);
+				fecharJFrame();
+			}
+		});
 		btnVoltar.setBounds(337, 0, 89, 23);
 		contentPane.add(btnVoltar);
 		
@@ -89,13 +122,40 @@ public class AdicionarCompra extends JFrame {
 		tf_CPF.setColumns(10);
 		
 		JLabel lblValorPagar = new JLabel("Valor \u00E0 Pagar:");
-		lblValorPagar.setBounds(27, 328, 82, 14);
+		lblValorPagar.setBounds(27, 378, 82, 14);
 		contentPane.add(lblValorPagar);
 		
 		tf_valorapagar = new JTextField();
 		tf_valorapagar.setEditable(false);
 		tf_valorapagar.setColumns(10);
-		tf_valorapagar.setBounds(100, 328, 104, 20);
+		tf_valorapagar.setBounds(100, 378, 104, 20);
 		contentPane.add(tf_valorapagar);
+		
+		JButton btnAdicionarLista = new JButton("Adicionar \u00E0 Lista");
+		btnAdicionarLista.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO: Coleta o ID.
+				//TODO: Busca o objeto servico no .buscarServico(String ID);
+				//TODO: Adiciona esse mesmo objeto ao model da lista list_Comprados.
+				
+				
+			}
+		});
+		btnAdicionarLista.setBounds(27, 325, 126, 23);
+		contentPane.add(btnAdicionarLista);
+		
+		JButton btnRemoverDaLista = new JButton("Remover da Lista");
+		btnRemoverDaLista.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO: remove o elemento da lista de comprados.
+			}
+		});
+		btnRemoverDaLista.setBounds(253, 328, 126, 23);
+		contentPane.add(btnRemoverDaLista);
+	}
+
+	protected void fecharJFrame() {
+		this.dispose();
+		
 	}
 }

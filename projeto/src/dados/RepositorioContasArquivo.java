d4f866c4289ed37681fada485a028ca8a9815143
unknown <fhab@g4c09.windows.cin.ufpe.br>
2015-01-26 17:26:30 -0300

    #1 Repositorio de Contas arquivo finalizado

--- projeto/src/dados/RepositorioContasArquivo.java
+++ projeto/src/dados/RepositorioContasArquivo.java
@@ -87,7 +87,7 @@ public  class RepositorioContasArquivo implements RepositorioContas{
 		}
 
 		contaSheet.removeRow(removingRow);
-		
+		rows--; //To fix the index counter at the Iterator.
 		try {
 			stream = new FileOutputStream("planilha.xls"); 
 		} catch (FileNotFoundException e) {
@@ -100,7 +100,7 @@ public  class RepositorioContasArquivo implements RepositorioContas{
 			wb.write(stream);
 			stream.close();
 		}catch(IOException e){
-			
+
 		}
 	}
 
@@ -144,6 +144,35 @@ public  class RepositorioContasArquivo implements RepositorioContas{
 	}
 
 	public void update(Conta conta) {
+		Conta ContaAntiga = buscar(conta.getCPF());
+		String[] accountData = new String[13];
+		accountData[0] = conta.getCPF();
+		accountData[1] = conta.getNome();
+		accountData[2] = conta.getEndereco().getBairro();
+		accountData[3] = conta.getEndereco().getCEP();
+		accountData[4] = conta.getEndereco().getCidade();
+		accountData[5] = conta.getEndereco().getEstado();
+		accountData[6]  = conta.getEndereco().getRua();
+		accountData[7] = conta.getEndereco().getComplemento();
+		accountData[8] = String.valueOf(conta.getEndereco().getNumero());
+		accountData[9] = conta.getCarro().getCor();
+		accountData[10] = conta.getCarro().getMarca();
+		accountData[11] = conta.getCarro().getModelo();
+		accountData[12] = conta.getCarro().getPlaca();
+		int index = 0;
+		
+		Iterator<Row> rowItr = contaSheet.rowIterator();
+		while(rowItr.hasNext()){
+			Row row = rowItr.next();
+			if(row.getCell(0).getStringCellValue().equals(conta.getCPF())){
+				Iterator<Cell> cells = row.cellIterator();
+				while(cells.hasNext()){
+					Cell cell = cells.next();
+					cell.setCellValue(accountData[index]);
+					index++;
+				}
+			}
+		}
 
 	}
 
